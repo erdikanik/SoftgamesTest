82795ee6045656c5565421c2e7908a730b6f6d1a
Erdi Kanik <erdikanik@gmail.com>
2022-05-07 15:25:39 +0300

    Create project

---
+++ SoftgamesTest/SoftgamesTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoftgamesTest
+//
+//  Created by Erdi on 7.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
